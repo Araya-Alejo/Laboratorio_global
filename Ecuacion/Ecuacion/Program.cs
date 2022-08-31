3486f511f5ea9016a88c2612fb80522c5d41b4da
Araya Alejo <103607905+Araya-Alejo@users.noreply.github.com>
2022-08-31 15:30:24 -0300

    .

--- Ecuacion/Ecuacion/Program.cs
+++ Ecuacion/Ecuacion/Program.cs
@@ -7,21 +7,19 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+ 
         string input;
         input = validate_equation();
         input = clean_equation(input);
 
-        Console.WriteLine(input);
-        Console.WriteLine((int)input.IndexOf("("));
-        Console.WriteLine((int)input.IndexOf(")"));
         string aux = input.Substring((int)input.IndexOf("("), (int)input.IndexOf(")")-1);
-        Console.WriteLine(solve_equation(aux));
+        
+        input = input.Substring(0, (int)input.IndexOf("("));
+        input = input + solve_equation(aux);
 
-
-
-
-
-        Console.WriteLine(aux);
+        input = solve_equation(input).ToString();
+        Console.WriteLine(input);
+        
 
     }
 
@@ -30,26 +28,25 @@ internal class Program
         string[] operators = { "+", "-", "*", "/" };
         int result = 0;
 
-        int operation;
+        foreach (string o in operators)
+        {
+            if (aux.Contains(o))
+            {
+                int value = aux.IndexOf(o);
+                Console.WriteLine("index of operation is " + value);
+                Console.WriteLine("operation is " + aux.Substring(value, value - 1));
+                if (aux.Substring(value, value - 1).Equals("-"))
+                {
+                    Console.WriteLine("fist number is " + aux.Substring(value - 1, value - 1));
+                    Console.WriteLine("second number is " + aux.Substring(value + 1, value - 1));
+                    Console.WriteLine();
 
-        if ((operation = aux.IndexOf("+")).Equals("+"))
-        {
-            result = Convert.ToInt32(aux.Substring(operation - 1, operation)) + Convert.ToInt32(aux.Substring(operation, operation + 1));
-        }
-        else if ((operation = aux.IndexOf("-")).Equals("-"))
-        {
-            result = Convert.ToInt32(aux.Substring(operation - 1, operation)) - Convert.ToInt32(aux.Substring(operation,operation + 1));
-        }
-        else if ((operation = aux.IndexOf("*")).Equals("*"))
-        {
-            result = Convert.ToInt32(aux.Substring(operation - 1, operation)) * Convert.ToInt32(aux.Substring(operation,operation + 1));
-        }
-        else if ((operation = aux.IndexOf("/")).Equals("/"))
-        {
-            result = Convert.ToInt32(aux.Substring(operation - 1, operation)) / Convert.ToInt32(aux.Substring(operation,operation + 1));
-        }
 
-        Console.WriteLine(result);
+                    result = Convert.ToInt32(aux.Substring(value - 1, value - 1)) - Convert.ToInt32(aux.Substring(value + 1, value - 1));
+                }
+            } 
+        }
+        Console.WriteLine("result is " + result);
         return result;
     }
 
@@ -62,11 +59,11 @@ internal class Program
 
     private static string validate_equation()
     {
-        string input;
+        string input = "2-(9-3)";
         do
         {
-            Console.WriteLine("Enter a equation");
-            input = Console.ReadLine();
+            /*Console.WriteLine("Enter a equation");
+            input = Console.ReadLine();*/
 
         } while (!Regex.IsMatch(input, pattern: @"^[0-9+/*() -]+$"));
         return input;
