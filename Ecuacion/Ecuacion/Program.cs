b2082bb276cb02daca67874554e09311a3bbc294
Asiel <asiel.alvarez@alumnos.frm.utn.edu.ar>
2022-09-08 17:47:22 -0300

    Validacion con Regex

--- Ecuacion/Ecuacion/Program.cs
+++ Ecuacion/Ecuacion/Program.cs
@@ -7,47 +7,21 @@ internal class Program
 {
     private static void Main(string[] args)
     {
- 
+
         string input;
         input = validate_equation();
         input = clean_equation(input);
+        char[] input_char = input.ToArray();
+        validate_structure(input_char);
 
-        string aux = input.Substring((int)input.IndexOf("("), (int)input.IndexOf(")")-1);
-        
-        input = input.Substring(0, (int)input.IndexOf("("));
-        input = input + solve_equation(aux);
+        //foreach (char i in input_char) Console.WriteLine(i);
 
-        input = solve_equation(input).ToString();
-        Console.WriteLine(input);
-        
 
     }
 
     private static int solve_equation(string aux)
     {
-        string[] operators = { "+", "-", "*", "/" };
-        int result = 0;
-
-        foreach (string o in operators)
-        {
-            if (aux.Contains(o))
-            {
-                int value = aux.IndexOf(o);
-                Console.WriteLine("index of operation is " + value);
-                Console.WriteLine("operation is " + aux.Substring(value, value - 1));
-                if (aux.Substring(value, value - 1).Equals("-"))
-                {
-                    Console.WriteLine("fist number is " + aux.Substring(value - 1, value - 1));
-                    Console.WriteLine("second number is " + aux.Substring(value + 1, value - 1));
-                    Console.WriteLine();
-
-
-                    result = Convert.ToInt32(aux.Substring(value - 1, value - 1)) - Convert.ToInt32(aux.Substring(value + 1, value - 1));
-                }
-            } 
-        }
-        Console.WriteLine("result is " + result);
-        return result;
+        return 1;
     }
 
     private static string clean_equation(string input)
@@ -67,5 +41,12 @@ internal class Program
 
         } while (!Regex.IsMatch(input, pattern: @"^[0-9+/*() -]+$"));
         return input;
+    }
+
+    private static void validate_structure(char[] input)
+    {
+        string input_string = "-x)(-";
+        Console.WriteLine(@"Regex[ '^)' ] : " + Regex.IsMatch(input_string, pattern: @"^\)+-*/|[-*=+/\(]$"));
+        //return input;
     }
 }
